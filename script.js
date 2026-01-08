5653effdb9fa25967c4cba19c88dc17c51e54b19
bubu139 <giakhanhtran88@gmail.com>
2026-01-08 20:56:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+
+function facebook() {
+    var url = "https://www.facebook.com/my.yy.241773/"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function inl() {
+    var url = "https://www.linkedin.com/in/ý-hồ-hình-mỹ-2a80a027a"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function insta() {
+    var url = "https://www.instagram.com/ys_mymoni/?igsh=ejhqeHZwbWMwZ2s%3D&utm_source=qr"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function tiktok() {
+    var url = "https://www.tiktok.com/@florasion_macrame?_r=1&_d=secCgYIASAHKAESPgo8YMHSI%2BMd5p5H1namA63JFlGlzJiwpqvCi%2F56d8e%2FluIl2A2PI2xFoRPF228qD4tG5u3NQgPj%2B73h2R9pGgA%3D&_svg=1&checksum=a99f80f0bb70ffb7b60cc371e69b691aaa2a5241cf8a3fcbd2f965231bec8e86&item_author_type=1&sec_uid=MS4wLjABAAAAr7mI1CiIvN4NMmBVdM7idyGgVRad-2irnhyv8GNJJGjY-9-NJnr0-BH3jlw80cA5&sec_user_id=MS4wLjABAAAAr7mI1CiIvN4NMmBVdM7idyGgVRad-2irnhyv8GNJJGjY-9-NJnr0-BH3jlw80cA5&share_app_id=1180&share_author_id=7297938480004416513&share_link_id=6E6BA1A9-C726-4B47-BE3F-7D9B5FCAB6CF&share_region=VN&share_scene=1&sharer_language=vi&social_share_type=4&source=h5_t&timestamp=1767545431&tt_from=copy&u_code=eb08ke3g9e1i54&ug_btm=b8727%2Cb0&user_id=7297938480004416513&utm_campaign=client_share&utm_medium=ios&utm_source=copy"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function read_all_01() {
+    window.location.href ="https://www.facebook.com/my.yy.241773/"; 
+}
+
+function read_all_02() {
+    window.location.href ="https://www.facebook.com/my.yy.241773/"; 
+}
+
+function flo() {
+    window.location.href ="https://www.facebook.com/my.yy.241773/"; 
+}
+
+function song() {
+    window.location.href ="https://www.facebook.com/my.yy.241773/"; 
+}
+
+function sach() {
+    window.location.href ="https://www.facebook.com/my.yy.241773/"; 
+}
+
+function MM() {
+    window.location.href ="https://www.facebook.com/my.yy.241773/"; 
+}
+
+
+function libary() {
+    window.location.href ="https://www.facebook.com/my.yy.241773/"; 
+}
+
+function about() {
+    window.location.href ="https://www.facebook.com/my.yy.241773/"; 
+}
+
+function project() {
+    window.location.href ="CVTEC.html"; 
+}
+
+function contact() {
+    window.location.href ="https://www.facebook.com/my.yy.241773/"; 
+    
+}
+
+function home() {
+    window.location.href ="index.html"; 
+    
+}
+
+// ---------------------------------------------------------------------------------------------
+// cvtec
+function cvtecfb() {
+    var url = "https://www.facebook.com/cvtenglishclub"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+
+function mtm(){
+    var url = "https://www.facebook.com/share/p/17yAHPw7sU/"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function esc(){
+    var url = "https://www.facebook.com/cvtenglishclub/posts/pfbid02iHyf1mtCXopWbTLHbdRs9Rysv75M7uX5DjTY5pCRfnbXSBvf7svh8LM8CMdFaFJGl?rdid=1apCmkN5cyr5UiSd"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
